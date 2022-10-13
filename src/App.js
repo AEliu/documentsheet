839c942608ec3443fc2a2e7b813757747bf5645c
AEliu <95649311@qq.com>
2022-10-14 06:50:51 +0800

    add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Form from "./components/form";
 
 function App() {
+  const [sheet, setSheet] = useState({
+    id: "",
+    heading: "",
+    issuer: "",
+    receiver: "",
+    copyReceiver: "",
+  });
+
+  const [sheets, setSheets] = useState(
+    JSON.parse(localStorage.getItem("localSheet")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("localSheet", JSON.stringify(sheets));
+  }, [sheets]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form sheet={sheet} setSheet={setSheet} sheets={sheets} setSheets={setSheets}/>
+      <div style={{whiteSpace: "pre-wrap"}}>
+        {sheets.map(s => <p>{s.heading}</p>)}
+      </div>
     </div>
   );
 }
---
+++ src/components/form.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+
+export default function Form({ sheet, setSheet, sheets, setSheets }) {
+  const [inerSheet, setInerSheet] = useState({
+    id: "",
+    heading: "",
+    issuer: "",
+    receiver: "",
+    copyReceiver: "",
+  });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    let value = event.target.value;
+//    value =  value.includes('\n') ? value.split('\n').join('<br />'): value;
+
+    console.log(event.target.value);
+    setInerSheet((values) => ({ ...values, [name]: value }));
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const timestamp = () => Date.now();
+    if (Object.values(inerSheet).every(x => x === '')) {
+        alert('❌没东西，不要打印')
+        return
+    }
+
+    const newSheet = {
+      ...inerSheet,
+      id: timestamp(),
+    };
+    setSheet(newSheet);
+    setSheets(sheets.concat(newSheet));
+
+    setInerSheet({
+        id: "",
+        heading: "",
+        issuer: "",
+        receiver: "",
+        copyReceiver: "",
+      });
+  };
+
+  return (
+    <>
+      <div className="form-group">
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            value={inerSheet.heading}
+            name="heading"
+            id=""
+            placeholder="标题……"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={inerSheet.issuer}
+            name="issuer"
+            id=""
+            placeholder="拟文……"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={inerSheet.receiver}
+            name="receiver"
+            id=""
+            placeholder="主送……"
+            onChange={handleChange}
+          />
+          <textarea
+            name="copyReceiver"
+            value={inerSheet.copyReceiver}
+            id=""
+            placeholder="抄送……"
+            onChange={handleChange}
+          >
+            
+          </textarea>
+          <button type="submit">
+            打印
+          </button>
+          <button type="reset">重置</button>
+        </form>
+      </div>
+    </>
+  );
+}
